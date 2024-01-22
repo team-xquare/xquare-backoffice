64e01b49690450ad701fad43a25022af0f6b8d0c
도경 <dokyungkim2926@gmail.com>
2024-01-22 23:44:18 +0900

    refactor :: logic

--- src/main/kotlin/com/xaquare/xquarebackoffice/excel/service/CreateExcelSheetService.kt
+++ src/main/kotlin/com/xaquare/xquarebackoffice/excel/service/CreateExcelSheetService.kt
@@ -12,7 +12,7 @@ class CreateExcelSheetService {
 
     fun execute(response: HttpServletResponse) {
         val workbook: Workbook = XSSFWorkbook()
-        val sheet: Sheet = workbook.createSheet("사용자 등록").apply {
+        val sheet: Sheet = workbook.createSheet("xquare").apply {
             defaultColumnWidth = 30
         }
 
@@ -29,8 +29,8 @@ class CreateExcelSheetService {
         val headerNames = arrayOf("이름", "입학년도", "생일", "학년", "반", "번호")
 
         val headerRow: Row = sheet.createRow(0)
-        headerNames.forEachIndexed { index, header ->
-            val headerCell: Cell = headerRow.createCell(index).apply {
+        headerNames.forEachIndexed { i, header ->
+            val headerCell: Cell = headerRow.createCell(i).apply {
                 setCellValue(header)
                 cellStyle = headerCellStyle
             }
@@ -45,15 +45,10 @@ class CreateExcelSheetService {
             arrayOf("ex) 김도경", "ex) 2023", "ex) 2007,09,19", "ex) 2", "ex) 4", "ex) 2"),
         )
 
-        bodyData.forEachIndexed { rowIndex, bodyRowData ->
-            // bodyData 배열의 각 행 에 대한 루프
-            val bodyRow: Row = sheet.createRow(rowIndex + 1)
-            // Excel 시트에 새로운 행을 생성하고 해당 행 객체를 bodyRow에 할당
-
-            bodyRowData.forEachIndexed { cellIndex, data ->
-                // 각 행(row)에 대한 데이터 배열(bodyRowData)의 각 열(column)에 대한 루프
-                val bodyCell: Cell = bodyRow.createCell(cellIndex).apply {
-                    // 현재 행(row)의 현재 열(column)에 대한 Excel 셀 객체를 생성하고 해당 셀 객체를 bodyCell 에 할당합니다.
+        bodyData.forEachIndexed { i, bodyRowData ->
+            val bodyRow: Row = sheet.createRow(i + 1)
+            bodyRowData.forEachIndexed { j, data ->
+                val bodyCell: Cell = bodyRow.createCell(j).apply {
                     setCellValue(data)
                     cellStyle = bodyCellStyle
                 }
@@ -62,7 +57,7 @@ class CreateExcelSheetService {
 
         //file
         val fileName = "spring_excel_download"
-        response.contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        response.contentType = "application/xquare.spreadsheetml.sheet"
         response.setHeader("Content-Disposition", "attachment;filename=$fileName.xlsx")
         val servletOutputStream = response.outputStream
 
