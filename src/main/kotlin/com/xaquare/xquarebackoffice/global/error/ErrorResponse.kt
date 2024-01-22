56a88d4514edc438e0de68c3cfae81e89da4e18b
도경 <dokyungkim2926@gmail.com>
2024-01-22 19:03:00 +0900

    add :: error

---
+++ src/main/kotlin/com/xaquare/xquarebackoffice/global/error/ErrorResponse.kt
@@ -0,0 +1,6 @@
+package com.xaquare.xquarebackoffice.global.error
+
+data class ErrorResponse(
+    val status: Int,
+    val message: String?
+)
---
+++ src/main/kotlin/com/xaquare/xquarebackoffice/global/error/exception/XquareException.kt
@@ -0,0 +1,5 @@
+package com.xaquare.xquarebackoffice.global.error.exception
+
+class XquareException(
+val errorCode: ErrorCode
+) : RuntimeException()
