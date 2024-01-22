c574afd49eddef2a3c2562083ea92cae0009fac8
도경 <dokyungkim2926@gmail.com>
2024-01-22 23:10:29 +0900

    setting :: apachi poi

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,8 +39,8 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
-	implementation("org.apache.poi', name: 'poi', version: '4.1.2")
-	implementation("org.apache.poi', name: 'poi-ooxml', version: '4.1.2")
+	implementation("org.apache.poi:poi:5.1.0")
+	implementation("org.apache.poi:poi-ooxml:5.1.0")
 }
 
 dependencyManagement {
