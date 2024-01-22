c36882abc0718dc23a7a4e6c2fa8e8ce64116294
soohyeon <meltapplee@gmail.com>
2024-01-22 17:31:28 +0900

    create :: project

---
+++ src/main/kotlin/com/xaquare/xquarebackoffice/XquareBackofficeApplication.kt
@@ -0,0 +1,11 @@
+package com.xaquare.xquarebackoffice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class XquareBackofficeApplication
+
+fun main(args: Array<String>) {
+	runApplication<XquareBackofficeApplication>(*args)
+}
---
+++ src/test/kotlin/com/xaquare/xquarebackoffice/XquareBackofficeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.xaquare.xquarebackoffice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class XquareBackofficeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
