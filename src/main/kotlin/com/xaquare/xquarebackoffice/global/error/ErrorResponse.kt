4dc31f44c99f1d16e440ed0b18fce269fc263662
도경 <dokyungkim2926@gmail.com>
2024-01-23 19:26:52 +0900

    git cached

--- src/main/kotlin/com/xaquare/xquarebackoffice/global/error/ErrorResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.xaquare.xquarebackoffice.global.error
-
-data class ErrorResponse(
-    val status: Int,
-    val message: String?
-)
--- src/main/kotlin/com/xaquare/xquarebackoffice/global/error/exception/XquareException.kt
+++
@@ -1,5 +0,0 @@
-package com.xaquare.xquarebackoffice.global.error.exception
-
-abstract class XquareException(
-val errorCode: ErrorCode
-) : RuntimeException()
