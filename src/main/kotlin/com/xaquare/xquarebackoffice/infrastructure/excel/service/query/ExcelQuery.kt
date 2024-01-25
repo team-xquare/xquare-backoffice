55e3fc35916440afc7d7d5bf97f094e520443b34
soohyeon <meltapplee@gmail.com>
2024-01-25 22:50:55 +0900

    add :: ExcelQuery

---
+++ src/main/kotlin/com/xaquare/xquarebackoffice/infrastructure/excel/service/query/ExcelQuery.kt
@@ -0,0 +1,13 @@
+package com.xaquare.xquarebackoffice.infrastructure.excel.service.query
+
+import org.springframework.stereotype.Component
+
+@Component
+class ExcelQuery {
+    fun executeQuery(): String {
+        return """
+            INSERT INTO excel (name, entrance_year, birth_day, grade, class_num, num)
+            VALUES (?, ?, ?, ?, ?, ?)
+        """
+    }
+}
