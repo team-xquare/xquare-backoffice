80cf109a87664f85c09fc5a0b4f3b783a36e4252
soohyeon <meltapplee@gmail.com>
2024-03-04 22:29:39 +0900

    add :: repository

---
+++ src/main/kotlin/com/xaquare/xquarebackoffice/domain/persistence/repository/UserRepository.kt
@@ -0,0 +1,7 @@
+package com.xaquare.xquarebackoffice.domain.persistence.repository
+
+import com.xaquare.xquarebackoffice.domain.entity.User
+import java.util.UUID
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserRepository: JpaRepository<User, UUID>
