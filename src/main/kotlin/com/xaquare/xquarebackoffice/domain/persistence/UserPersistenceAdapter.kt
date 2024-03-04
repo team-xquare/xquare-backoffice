28c2f9bf8caf209d4036d73d8941e4cb5ae036ff
soohyeon <meltapplee@gmail.com>
2024-03-04 22:41:03 +0900

    add :: adapter

---
+++ src/main/kotlin/com/xaquare/xquarebackoffice/domain/persistence/UserPersistenceAdapter.kt
@@ -0,0 +1,14 @@
+package com.xaquare.xquarebackoffice.domain.persistence
+
+import com.xaquare.xquarebackoffice.domain.entity.User
+import com.xaquare.xquarebackoffice.domain.persistence.repository.UserRepository
+import org.springframework.stereotype.Component
+
+@Component
+class UserPersistenceAdapter(
+    private val userRepository: UserRepository
+) {
+    fun findAll(): List<User> {
+        return userRepository.findAll()
+    }
+}
